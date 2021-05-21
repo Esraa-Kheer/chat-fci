7d334a58a68c8657d865cdd814364b9188c952c7
dinaomran28 <you@example.com>
2021-05-21 22:25:23 +0200

    add desgin in index.html & 2 lines in index.js

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const express = require("express");
+const app = require("express")();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
-io.on('connection', (socket) => {
-  socket.on('chat message', msg => {
-    io.emit('chat message', msg);
+app.use(express.static(__dirname + "/images"));
+
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
   });
 });
 
